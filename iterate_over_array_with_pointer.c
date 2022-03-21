bdcf624f6222312d71701bf2ee8d882ec30d0727
Uladzislau Khamkou <kham@tuta.io>
2022-03-21 16:52:14 +0300

    implement iteration over diagonal of a matrix

---
+++ iterate_over_array_with_pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define max_array_size 4
+
+int A[max_array_size][max_array_size];
+int main() {
+	int n;
+	printf("Size of an matrix: ");
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		printf("row #%d: ", i + 1);
+		for (int *p = *(A + i); p < *(A + i) + n; p++)
+			scanf("%d", p);
+	}
+
+	int *diag_end = *A + n * max_array_size - 1;
+	for (int *p = *A + n - 1; p < diag_end; p += max_array_size - 1)
+		printf("%p -> %d\n", p, *p);
+}
