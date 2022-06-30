f58614e7eb3639bd72cc4e84c2e8d020849a1d4a
Uladzislau Khamkou <kham@tuta.io>
2022-06-30 10:44:01 +0300

    use keyword to store a variable in a register

--- echo.c
+++ echo.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
 	char s[256];
-	while (printf(">>> "), scanf(" %255[^\n]", s) == 1) {
-		printf("%s\n", s);
+	register char *reg = s;
+	while (printf(">>> "), scanf(" %255[^\n]", reg) == 1) {
+		printf("%s\n", reg);
 	}
 }
