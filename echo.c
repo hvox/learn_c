3e0d79938d2f348e1a6ab3a4ec901e554380d57b
Uladzislau Khamkou <kham@tuta.io>
2022-07-05 15:36:43 +0300

    uppercase letters in echo

--- echo.c
+++ echo.c
@@ -3,6 +3,8 @@ int main() {
 	char s[256];
 	register char *reg = s;
 	while (printf(">>> "), scanf(" %255[^\n]", reg) == 1) {
+		for (char *ch = s; *ch; ch++)
+			*ch -= (*ch > 96 && *ch < 123) ? 32 : 0;
 		printf("%s\n", reg);
 	}
 }
